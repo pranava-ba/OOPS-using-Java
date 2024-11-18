bd7591e2699e6b5ebf8830f32ced81562d92bc2d
BA Pranava <97015345+pranava-ba@users.noreply.github.com>
2024-11-18 18:03:04 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+    System.out.println("Hello World i am going to ace OOPS");
+    System.out.println("I am learning from Mosh");
+    }
+}
